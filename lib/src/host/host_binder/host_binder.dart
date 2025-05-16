97e153b9c7f17c2501da8d8c7e2b09bd193f777f
Omar Elnemr <omarelnemr8@gmail.com>
2025-05-16 18:09:39 +0300

    Implement static file server functionality in HostBinder

--- lib/src/host/host_binder/host_binder.dart
+++ lib/src/host/host_binder/host_binder.dart
@@ -1,18 +1,37 @@
+import 'dart:developer';
+import 'dart:io';
 
-class HostParameters{
-  final String host;
-  final int port;
-  HostParameters({
-    required this.host,
-    required this.port,
-  });
-}
+import 'package:shelf/shelf.dart';
+import 'package:sqflite_live/src/host/file_manager/file_manager.dart';
+import 'package:sqflite_live/src/host/host_binder/host_parameters.dart';
+import 'package:shelf/shelf_io.dart'    as shelf_io;
+import 'package:shelf_static/shelf_static.dart';
+
+import '../../exceptions/server_failure.dart';
 class HostBinder{
-  Future<void> host([void data]) async {
+  final HostParameters hostParameters;
+  HttpServer? server;
+  final FileManager fileManager;
+  HostBinder(this.hostParameters, this.fileManager);
+  host(String hostDir) async {
+   try {
+      // Serve everything under assets/web/ (make sure you've
+      // declared that folder in your pubspec under flutter.assets)
+      final staticHandler = createStaticHandler(
+        hostDir,
+        defaultDocument: 'index.html',
+      );
 
+      final handler = Pipeline().addMiddleware(logRequests()).addHandler(staticHandler);
 
+      server = await shelf_io.serve(
+          handler, InternetAddress.anyIPv4, hostParameters.port);
+      print('🗂️  Static server running at http://${server?.address.address}:${hostParameters.port}/index.html?url=${fileManager.dbName}.db/');
+    }catch(e,s){
+     log('e: $e,$s');
+     throw(ServerFailure(e.toString(),stackTrace: s));
+   }
   }
-
   flush(){
 
   }
